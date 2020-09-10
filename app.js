7a51f84b8dc37077a1bf9f598ffd0642cffde8a0
jackass <Edes2128>
2020-09-10 10:38:21 +0200

    NODE

--- app.js
+++ app.js
@@ -856,7 +856,7 @@ app.post('/website', async (req, res) => {
             <h3 style="text-align:center;color:dodgerblue">Website</h3>
             <div style="display:flex">
             <p><b>Topologjia:</b></p>
-            <p>${req.body.topologjia === 'reference' ? req.body.linkRefer : req.body.topologjia}</p>
+            <p>${req.body.topologjia === 'reference' ? `Web Reference: ${req.body.linkRefer}` : req.body.topologjia}</p>
             </div>
             <div style="display:flex">
             <p><b>Dizajni i Webit:</b></p>
